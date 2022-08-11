180d50bdcf4f131bf8d0ac82cf3918b7a9d1004e
exectble <92788415+exectble@users.noreply.github.com>
2022-08-12 02:21:58 +0300

    tasks12

---
+++ DoneExercises/labs12/task_12_3.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Задание 12.3
+
+Создать функцию print_ip_table, которая отображает таблицу доступных
+и недоступных IP-адресов.
+
+Функция ожидает как аргументы два списка:
+* список доступных IP-адресов
+* список недоступных IP-адресов
+
+Результат работы функции - вывод на стандартный поток вывода таблицы вида:
+
+Reachable    Unreachable
+-----------  -------------
+10.1.1.1     10.1.1.7
+10.1.1.2     10.1.1.8
+             10.1.1.9
+
+"""
+
+from tabulate import tabulate
+
+def print_ip_table(ip_reachable, ip_unreachable):
+    table = {'Reachable': ip_reachable, 'Unreachable': ip_unreachable}
+    print(tabulate(table, headers='keys'))
